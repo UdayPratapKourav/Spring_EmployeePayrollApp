b862dd10c7ff8ada157082736ed08c89b311192d
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-02-27 15:45:05 +0530

    [Uday] Add. apply name and salary validation

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/DTO/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/DTO/EmployeePayrollDTO.java
@@ -1,7 +1,14 @@
 package com.bridgelabz.employeepayrollapp.DTO;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+   @NotEmpty(message = "Employee can not be null")
+   @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Employee name invalid")
     public String name;
+   @Min(value = 500,message = "Min salary should be more then 500")
     public long salary;
 
     public EmployeePayrollDTO(String name,long salary){
