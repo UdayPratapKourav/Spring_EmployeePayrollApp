4e3473a7d6d5982aa8a7215308b12d0562d4c014
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-02-28 15:48:37 +0530

    [Uday] Update. employeepayrolldto

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/DTO/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/DTO/EmployeePayrollDTO.java
@@ -3,20 +3,32 @@ package com.bridgelabz.employeepayrollapp.DTO;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import lombok.ToString;
 
-public class EmployeePayrollDTO {
+import java.util.List;
+
+public @ToString class EmployeePayrollDTO {
    @NotEmpty(message = "Employee can not be null")
    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Employee name invalid")
     public String name;
+
    @Min(value = 500,message = "Min salary should be more then 500")
     public long salary;
 
-    public EmployeePayrollDTO(String name,long salary){
-        this.name=name;
-        this.salary=salary;
-    }
+    public String gender;
+    public String startDate;
+    public String note;
+    public String profilePic;
+    public List<String> department;
 
-    public String toString(){
-        return "Name : "+name+" Salary : "+salary;
-    }
+
+//
+//    public EmployeePayrollDTO(String name,long salary){
+//        this.name=name;
+//        this.salary=salary;
+//    }
+//
+//    public String toString(){
+//        return "Name : "+name+" Salary : "+salary;
+//    }
 }
