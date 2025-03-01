3cdc9af4caefb1e55d8e16c02785ad41bcdcd887
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-03-01 15:30:37 +0530

    [Uday] Update. add method for department

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -37,6 +37,13 @@ public class EmployeePayrollController {
         return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.OK);
     }
 
+    @GetMapping("department/{department}")
+    public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("department")String department){
+        List<EmployeePayrollData> empDataList=null;
+        empDataList= employeePayrollService.getEmployeesByDepartment(department);
+        ResponseDTO responseDTO= new ResponseDTO("Get call for department successful",empDataList);
+        return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.OK);
+    }
 
 
     @PostMapping("/create")
